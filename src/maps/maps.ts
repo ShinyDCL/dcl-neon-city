6ee02b5c76d6bc24ef7fc1b80d7829bce8dcec6f
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2024-05-29 23:53:11 +0300

    feat: add map for level 3

--- src/maps/maps.ts
+++ src/maps/maps.ts
@@ -1,5 +1,6 @@
 import { Tile } from '../tile'
 import { level1 } from './level1'
 import { level2 } from './level2'
+import { level3 } from './level3'
 
-export const maps: Tile[][][] = [level1, level2]
+export const maps: Tile[][][] = [level1, level2, level3]
