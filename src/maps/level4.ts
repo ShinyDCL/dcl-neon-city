80e3a62c8cf5b701e118cfd4a7abe1497e945fa6
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2024-05-31 14:39:20 +0300

    feat: re-order maps by difficulty

--- src/maps/level4.ts
+++ src/maps/level4.ts
@@ -2,614 +2,198 @@ import { Tile } from '../tile'
 
 export const level4: Tile[][] = [
   [
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [0, 180]
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HS'
-    }
+    { tileType: 'HM' },
+    { tileType: 'HM' },
+    { tileType: 'HS' },
+    { tileType: 'HL' },
+    { tileType: 'HS' },
+    { tileType: 'HM' },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'HM' },
+    { tileType: 'HM' },
+    { tileType: 'HM' },
+    { tileType: 'HM' },
+    { tileType: 'HM' },
+    { tileType: 'HS' }
   ],
   [
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [180, 270]
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [90]
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [0]
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [90, 270]
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [90]
-    },
-    {
-      tileType: 'HM'
-    }
+    { tileType: 'HM' },
+    { tileType: 'RT' },
+    { tileType: 'RS' },
+    { tileType: 'R3W' },
+    { tileType: 'RT' },
+    { tileType: 'RS' },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [180, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'RT', partOfWinningPath: true, validRotations: [90] },
+    { tileType: 'HL' }
   ],
   [
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'R3W'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'R3W'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [180, 270]
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [90, 270]
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [90, 180]
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [0, 180]
-    },
-    {
-      tileType: 'HL'
-    }
+    { tileType: 'HL' },
+    { tileType: 'RS' },
+    { tileType: 'HL' },
+    { tileType: 'RS' },
+    { tileType: 'HM' },
+    { tileType: 'HS' },
+    { tileType: 'HL' },
+    { tileType: 'RT' },
+    { tileType: 'HS' },
+    { tileType: 'HS' },
+    { tileType: 'HL' },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'HL' }
   ],
   [
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'OS'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'OM'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [0, 270]
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [180]
-    },
-    {
-      tileType: 'HL'
-    }
+    { tileType: 'HS' },
+    { tileType: 'R3W' },
+    { tileType: 'RS' },
+    { tileType: 'R3W' },
+    { tileType: 'RS' },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 270] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [90, 180] },
+    { tileType: 'HL' }
   ],
   [
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'OS'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'OS'
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [0, 180]
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HM'
-    }
+    { tileType: 'HS' },
+    { tileType: 'RT' },
+    { tileType: 'HM' },
+    { tileType: 'OM' },
+    { tileType: 'OM' },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'OL' },
+    { tileType: 'OS' },
+    { tileType: 'OS' },
+    { tileType: 'RS' },
+    { tileType: 'HS' },
+    { tileType: 'RS' },
+    { tileType: 'HL' }
   ],
   [
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [0]
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [90, 270]
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [90]
-    },
-    {
-      tileType: 'OM'
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [0]
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [90, 270]
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [90]
-    },
-    {
-      tileType: 'OM'
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [0]
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [90, 180]
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'HM'
-    }
+    { tileType: 'HL' },
+    { tileType: 'RT', partOfWinningPath: true, validRotations: [0, 270] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [90, 180] },
+    { tileType: 'R3W' },
+    { tileType: 'RT' },
+    { tileType: 'R3W' },
+    { tileType: 'OS' },
+    { tileType: 'RS' },
+    { tileType: 'R3W' },
+    { tileType: 'HL' }
   ],
   [
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [0, 180]
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [180, 270]
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [90, 270]
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [90, 180]
-    },
-    {
-      tileType: 'OM'
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [180, 270]
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [90, 270]
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [90, 180]
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'HM'
-    }
+    { tileType: 'HL' },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'HS' },
+    { tileType: 'RT' },
+    { tileType: 'OS' },
+    { tileType: 'OS' },
+    { tileType: 'RT' },
+    { tileType: 'OS' },
+    { tileType: 'RS' },
+    { tileType: 'OL' },
+    { tileType: 'HS' },
+    { tileType: 'RS' },
+    { tileType: 'HL' }
   ],
   [
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [270]
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [0, 90]
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'OM'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'HL'
-    }
+    { tileType: 'HL' },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [180, 270] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 90] },
+    { tileType: 'R3W' },
+    { tileType: 'RS' },
+    { tileType: 'RT' },
+    { tileType: 'R3W' },
+    { tileType: 'HS' }
   ],
   [
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [0, 180]
-    },
-    {
-      tileType: 'OM'
-    },
-    {
-      tileType: 'OS'
-    },
-    {
-      tileType: 'OM'
-    },
-    {
-      tileType: 'OM'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'OS'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HS'
-    }
+    { tileType: 'HM' },
+    { tileType: 'RS' },
+    { tileType: 'HL' },
+    { tileType: 'OS' },
+    { tileType: 'RS' },
+    { tileType: 'OM' },
+    { tileType: 'OL' },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'OL' },
+    { tileType: 'OL' },
+    { tileType: 'HM' },
+    { tileType: 'RT' },
+    { tileType: 'HM' }
   ],
   [
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [180, 270]
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [90, 270]
-    },
-    {
-      tileType: 'RT',
-      partOfWinningPath: true,
-      validRotations: [90]
-    },
-    {
-      tileType: 'OS'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'OL'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'HM'
-    }
+    { tileType: 'HM' },
+    { tileType: 'HS' },
+    { tileType: 'RS' },
+    { tileType: 'R3W' },
+    { tileType: 'RS' },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 270] },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [90, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [90, 180] },
+    { tileType: 'RS' },
+    { tileType: 'R3W' },
+    { tileType: 'RS' },
+    { tileType: 'R3W' },
+    { tileType: 'HM' }
   ],
   [
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [0, 180]
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'HM'
-    }
+    { tileType: 'HM' },
+    { tileType: 'RS' },
+    { tileType: 'HM' },
+    { tileType: 'RT' },
+    { tileType: 'HM' },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'HS' },
+    { tileType: 'HM' },
+    { tileType: 'HL' },
+    { tileType: 'RT' },
+    { tileType: 'HL' },
+    { tileType: 'RS' },
+    { tileType: 'HL' }
   ],
   [
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'R3W',
-      partOfWinningPath: true,
-      validRotations: [180, 270]
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [90, 270]
-    },
-    {
-      tileType: 'R4W',
-      partOfWinningPath: true,
-      validRotations: [0, 90, 180, 270]
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'R3W'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'RS'
-    },
-    {
-      tileType: 'RT'
-    },
-    {
-      tileType: 'HS'
-    }
+    { tileType: 'HS' },
+    { tileType: 'RT' },
+    { tileType: 'RS' },
+    { tileType: 'R3W' },
+    { tileType: 'RS' },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [180, 270] },
+    { tileType: 'R3W', partOfWinningPath: true, validRotations: [0, 90] },
+    { tileType: 'RS' },
+    { tileType: 'RS' },
+    { tileType: 'R3W' },
+    { tileType: 'RS' },
+    { tileType: 'RS' },
+    { tileType: 'HS' }
   ],
   [
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'RS',
-      partOfWinningPath: true,
-      validRotations: [0, 180]
-    },
-    {
-      tileType: 'HM'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HS'
-    },
-    {
-      tileType: 'HL'
-    },
-    {
-      tileType: 'HL'
-    }
+    { tileType: 'HS' },
+    { tileType: 'HS' },
+    { tileType: 'HL' },
+    { tileType: 'HM' },
+    { tileType: 'HL' },
+    { tileType: 'HL' },
+    { tileType: 'RS', partOfWinningPath: true, validRotations: [0, 180] },
+    { tileType: 'HS' },
+    { tileType: 'HL' },
+    { tileType: 'HM' },
+    { tileType: 'HL' },
+    { tileType: 'HM' },
+    { tileType: 'HL' }
   ]
 ] as Tile[][]
